de57392e938283696111367c3a6bff5ab06d4985
Leonardo Carvalho <leovilela100@gmail.com>
2022-10-14 00:40:03 -0300

    Subindo novas modificações da API_JAVA

--- 09_Padrao_De_Projetos_Value_Object/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 09_Padrao_De_Projetos_Value_Object/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -1,7 +1,6 @@
 package br.com.erudio.controllers;
 
-
-import br.com.erudio.model.Person;
+import br.com.erudio.data.vo.v1.PersonVO;
 import br.com.erudio.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -18,25 +17,25 @@ public class PersonController {
     private PersonServices service;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Person> findAll() {
+    public List<PersonVO> findAll() {
         return service.findAll();
     }
 
     @GetMapping(value = "/{id}",
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathVariable(value = "id") Long id) {
+    public PersonVO findById(@PathVariable(value = "id") Long id) {
         return service.findById(id);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person create(@RequestBody Person person) {
+    public PersonVO create(@RequestBody PersonVO person) {
         return service.create(person);
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person update(@RequestBody Person person) {
+    public PersonVO update(@RequestBody PersonVO person) {
         return service.update(person);
     }
 
---
+++ 11_Adicionando_Suporte_a_Migrations_Com_Flyway/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,10 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+(1, 'Planeta Sayadin', 'Vegeta', 'Male', 'Sayadin'),
+(2, 'Uberlandia - Minas Gerais - Brasil', 'Claudiene atualizada', 'Female', 'Silva'),
+(5, 'Uberlandia - Minas Gerais - Brasil', 'Teste', 'Female', 'Silva'),
+(6, 'Uberlandia - Minas Gerais - Brasil', 'Leonardo Atualizado 22 Bolsonaro', 'Male', 'Carvalho Vilela'),
+(8, 'Uberlandia - Minas Gerais - Brasil', 'Leonardo Atualizado 22', 'Male', 'Carvalho Vilela'),
+(9, 'Planeta Sayadin', 'Vegeta', 'Male', 'Sayadin'),
+(19, 'Planeta Sayadin', 'Goku', 'Male', 'Sayadin'),
+(20, 'Record', 'Sambariolive', 'Male', 'Bonitinho'),
+(21, 'Record', 'Ze ', 'Male', 'Bonitinho');
